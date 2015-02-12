8067d08db4e5f3346addf3a9ba556ab6c807013f
yassu <mathyassu@gmail.com>
2015-02-12 11:26:17 +0900

    __FILE_?_ == zsh prefix

--- src/timer.rb
+++ src/timer.rb
@@ -34,70 +34,73 @@ def get_datedelta_format(sec, min, hour)
   end
 end
 
-# build sec, min,  hour, and message variable
-message = nil
-sec, min, hour = nil, nil, nil
-OptionParser.new do |opt|
-  opt.on('-s VALUE', '--second VALUE') do |s|
-    begin
-      sec = Integer(s)
-    rescue ArgumentError => ex
-      print("Error: second value is not defined.\n")
+if __FILE__ == $0
+  # build sec, min,  hour, and message variable
+  message = nil
+  sec, min, hour = nil, nil, nil
+  OptionParser.new do |opt|
+    opt.on('-s VALUE', '--second VALUE') do |s|
+      begin
+        sec = Integer(s)
+      rescue ArgumentError => ex
+        print("Error: second value is not defined.\n")
+      end
     end
-  end
 
-  opt.on('-m VALUE', '--minute VALUE') do |m|
-    begin
-      min = Integer(m)
-    rescue ArgumentError => ex
-      print("Error: minute value is not defined.\n")
+    opt.on('-m VALUE', '--minute VALUE') do |m|
+      begin
+        min = Integer(m)
+      rescue ArgumentError => ex
+        print("Error: minute value is not defined.\n")
+      end
     end
-  end
 
-  opt.on('-h VALUE', '--hour VALUE') do |h|
-    begin
-      hour = Integer(h)
-    rescue ArgumentError => ex
-      print("Error: hour value is not defined.\n")
+    opt.on('-h VALUE', '--hour VALUE') do |h|
+      begin
+        hour = Integer(h)
+      rescue ArgumentError => ex
+        print("Error: hour value is not defined.\n")
+      end
     end
+
+    opt.on('--msg VALUE', '--message VALUE') do |msg|
+      message = msg
+    end
+
+    opt.on('-v', '--version') do |v|
+      print "Version: #{VERSION}\n"
+    end
+
+    opt.parse!(ARGV)
   end
 
-  opt.on('--msg VALUE', '--message VALUE') do |msg|
-    message = msg
-  end
-
-  opt.on('-v', '--version') do |v|
-    print "Version: #{VERSION}\n"
-  end
-
-  opt.parse!(ARGV)
-end
-
-if sec or min or hour
-  sec = (sec)? sec: 0
-  min = (min)? min: 0
-  hour = (hour)? hour: 0
-  datedelta_s = get_datedelta_format(sec, min, hour)
-  time = hour * 60 * 60 + min * 60 + sec
-  sleep time
-  if message
-    print("#{message}\n")
-  else
-    print("It spent #{datedelta_s}.\n")
-  end
-
-elsif ARGV.size == 1
-  begin
-    time = Integer(ARGV[0])
+  if sec or min or hour
+    sec = (sec)? sec: 0
+    min = (min)? min: 0
+    hour = (hour)? hour: 0
+    datedelta_s = get_datedelta_format(sec, min, hour)
+    time = hour * 60 * 60 + min * 60 + sec
     sleep time
-    sec_s = append_multiple_prefix(time, 'second')
-    print("It spent #{sec_s}.\n")
-  rescue ArgumentError => ex
-    print("Error: seconds is not defined.\n")
+    if message
+      print("#{message}\n")
+    else
+      print("It spent #{datedelta_s}.\n")
+    end
+
+  elsif ARGV.size == 1
+    begin
+      time = Integer(ARGV[0])
+      sleep time
+      sec_s = append_multiple_prefix(time, 'second')
+      print("It spent #{sec_s}.\n")
+    rescue ArgumentError => ex
+      print("Error: seconds is not defined.\n")
+      exit
+    end
+    if not time
+      print("Illegal integer format.\n")
+    end
+  else
+    print("Illegal input.\n")
   end
-  if not time
-    print("Illegal integer format.\n")
-  end
-else
-  print("Illegal input.\n")
 end
